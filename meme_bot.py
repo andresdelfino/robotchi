b7a1f7a1acb3bf7347172ea652ce288cd83e5f80
Andre Delfino <adelfino@gmail.com>
2022-09-27 16:22:30 -0300

    Add messirve

--- meme_bot.py
+++ meme_bot.py
@@ -2,7 +2,7 @@ import random
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from config import BOT_TOKEN
 from tools import *
-from os import getcwd, path, listdir
+from os import environ, getcwd, path, listdir
 
 
 def start(update, context):  # Update is an object that represents an incoming update sent via a chat
@@ -36,14 +36,25 @@ def get_meme(update, context):
     log_command("/meme", str(update.message.from_user['username']))
 
 
+def get_messirve(update, context):
+    print('We are inside get_messirve function')
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=open(path.join(getcwd(), 'memes', 'messirve.jpg'), 'rb')
+    )
+    log_command('/messirve', str(update.message.from_user['username']))
+
+
 start_handler = CommandHandler('start', start)  # CommandHandler is a class that defines what happens when a user
 # executes a command (/command). In this case, when user sends "start" command, the start function will be called
 echo_handler = MessageHandler(Filters.text & (~ Filters.command), echo)  # MessageHandler is a class used when we
 # need to handle telegram messages. They might contain text, media or status updates
 meme_handler = CommandHandler('meme', get_meme)
+messirve_handler = CommandHandler('messirve', get_messirve)
 
 if __name__ == "__main__":
-    updater = Updater(token=BOT_TOKEN)  # Updater class, which employs the class Dispatcher, provides a frontend to the
+    token = environ.get('BOT_TOKEN', BOT_TOKEN)
+    updater = Updater(token=token)  # Updater class, which employs the class Dispatcher, provides a frontend to the
     # class Bot to the programmer, so they can focus on coding the bot. Its purpose is to receive the updates from
     # Telegram and to deliver them to said dispatcher. It also runs in a separate thread, so the user can interact with
     # the bot, for example on the command line.
@@ -53,4 +64,5 @@ if __name__ == "__main__":
     dispatcher.add_handler(start_handler, 0)  # Register a handler to the dispatcher. Order and priority counts.
     dispatcher.add_handler(echo_handler, 0)
     dispatcher.add_handler(meme_handler, 0)
+    dispatcher.add_handler(messirve_handler, 0)
     updater.start_polling()
