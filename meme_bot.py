3d85a70444f85e395b504c9e9f18c59a51a17e6d
Andrés Delfino <adelfino@gmail.com>
2022-09-29 09:03:58 -0300

    Simplify inline query code

--- meme_bot.py
+++ meme_bot.py
@@ -1,3 +1,4 @@
+import pprint
 import random
 import uuid
 from telegram import InlineQueryResultPhoto
@@ -10,6 +11,63 @@ from tags import tag_dict
 import urllib.request
 from bs4 import BeautifulSoup
 import re
+
+
+memes = [
+    {
+        'id': str(uuid.uuid4()),
+        'name': 'aliens',
+        'photo_url': 'https://www.meme-arsenal.com/memes/6b8f460f481aa48441d8dc6d88d0a041.jpg',
+        'thumb_url': 'https://www.meme-arsenal.com/memes/6b8f460f481aa48441d8dc6d88d0a041.jpg',
+        'tags': {
+            'aliens',
+        }
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'name': 'but_it_s_honest_work',
+        'photo_url': 'https://www.meme-arsenal.com/memes/333258e655f5d613b7ee4a0663e0506d.jpg',
+        'thumb_url': 'https://www.meme-arsenal.com/memes/333258e655f5d613b7ee4a0663e0506d.jpg',
+        'tags': {
+            *"but it's honest work".split(),
+        }
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'name': 'not_sure_if',
+        'photo_url': 'https://www.meme-arsenal.com/memes/389f398c7bf55ae32a8a326031af2c32.jpg',
+        'thumb_url': 'https://www.meme-arsenal.com/memes/389f398c7bf55ae32a8a326031af2c32.jpg',
+        'tags': {
+            'fry',
+            *'not sure if'.split(),
+            'futurama',
+        },
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'name': 'shut_up_and_take_my_money',
+        'photo_url': 'https://www.meme-arsenal.com/memes/ed3a49701d7dce8d9d1cb5f74a7f79f8.jpg',
+        'thumb_url': 'https://www.meme-arsenal.com/memes/ed3a49701d7dce8d9d1cb5f74a7f79f8.jpg',
+        'tags': {
+            'fry',
+            *'shut up and take my money'.split(),
+            'futurama',
+        }
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'name': 'spider_man_pointing_at_spider_man',
+        'photo_url': 'https://www.meme-arsenal.com/memes/2332a9b45fea20c7f92ea5324dd6be49.jpg',
+        'thumb_url': 'https://www.meme-arsenal.com/memes/2332a9b45fea20c7f92ea5324dd6be49.jpg',
+        'tags': {
+            'spiderman',
+            'spider-man',
+            'doble',
+            'duplicado',
+            'igual',
+        }
+    },
+]
 
 
 def start(update, context):  # Update is an object that represents an incoming update sent via a chat
@@ -81,72 +139,43 @@ def get_wiki(update, context):
 
 
 def inline_query(update, context):  # parameter context is not used.
-    print("We are inside inline_query function")
+    # https://core.telegram.org/bots/api#inlinequeryresultphoto
+
+    # https://docs.python-telegram-bot.org/en/v13.14/telegram.ext.inlinequeryhandler.html
+    # https://docs.python-telegram-bot.org/en/v13.14/telegram.inlinequery.html
+    # https://docs.python-telegram-bot.org/en/v13.14/telegram.inlinequeryresultphoto.html
+
+    print('We are inside inline_query function')
 
     query = update.inline_query.query
 
+    print('User:', update.inline_query.from_user.username)
+    print('Query:', query)
+
     if query == '':
         return
-
-    memes = {  # PEP8: variables should be lowercase
-        'aliens': {
-            'photo_url': 'https://www.meme-arsenal.com/memes/6b8f460f481aa48441d8dc6d88d0a041.jpg',
-            'thumb_url': 'https://www.meme-arsenal.com/memes/6b8f460f481aa48441d8dc6d88d0a041.jpg',
-            'tags': {
-                'aliens',
-            }
-        },
-        'but_it_s_honest_work': {
-            'photo_url': 'https://www.meme-arsenal.com/memes/333258e655f5d613b7ee4a0663e0506d.jpg',
-            'thumb_url': 'https://www.meme-arsenal.com/memes/333258e655f5d613b7ee4a0663e0506d.jpg',
-            'tags': {
-                *"but it's honest work".split(),
-            }
-        },
-        'not_sure_if': {
-            'photo_url': 'https://www.meme-arsenal.com/memes/389f398c7bf55ae32a8a326031af2c32.jpg',
-            'thumb_url': 'https://www.meme-arsenal.com/memes/389f398c7bf55ae32a8a326031af2c32.jpg',
-            'tags': {
-                'fry',
-                *'not sure if'.split(),
-                'futurama',
-            },
-        },
-        'shut_up_and_take_my_money': {
-            'photo_url': 'https://www.meme-arsenal.com/memes/ed3a49701d7dce8d9d1cb5f74a7f79f8.jpg',
-            'thumb_url': 'https://www.meme-arsenal.com/memes/ed3a49701d7dce8d9d1cb5f74a7f79f8.jpg',
-            'tags': {
-                'fry',
-                *'shut up and take my money'.split(),
-                'futurama',
-            }
-        },
-        'spider_man_pointing_at_spider_man': {
-            'photo_url': 'https://www.meme-arsenal.com/memes/2332a9b45fea20c7f92ea5324dd6be49.jpg',
-            'thumb_url': 'https://www.meme-arsenal.com/memes/2332a9b45fea20c7f92ea5324dd6be49.jpg',
-            'tags': {
-                'spiderman',
-                'spider-man',
-                'doble',
-            }
-        },
-    }
 
     results = []
 
     tags = set(query.lower().split())
 
-    for meme_name, meme_data in memes.items():
-        if query == 'all' or tags <= meme_data['tags']:
+    print('Memes matched:')
+
+    for meme in memes:
+        if query == 'all' or tags <= meme['tags']:
+            print('*', meme['name'])
             results.append(
                 InlineQueryResultPhoto(
-                    id=str(uuid.uuid4()),
-                    photo_url=meme_data['photo_url'],
-                    thumb_url=meme_data['thumb_url'],
+                    id=meme['id'],
+                    photo_url=meme['photo_url'],
+                    thumb_url=meme['thumb_url'],
                 ),
             )
 
-    update.inline_query.answer(results)
+    if not results:
+        print('None')
+
+    update.inline_query.answer(results, cache_time=0)
 
 
 start_handler = CommandHandler('start', start)  # CommandHandler is a class that defines what happens when a user
